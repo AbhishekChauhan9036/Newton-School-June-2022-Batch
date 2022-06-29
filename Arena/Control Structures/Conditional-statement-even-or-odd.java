727a951c28af8234900f811ac12feaede6ea1a96
Abhi <abhishekchauhan9036@gmail.com>
2022-06-29 06:29:42 +0530

    commit message

---
+++ Arena/Control Structures/Conditional-statement-even-or-odd.java
@@ -0,0 +1,24 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+import java.util.*;
+import java.io.*;
+class Main {
+	public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N%2==0){
+            System.out.println(0);
+		}else{
+    		System.out.println(1);
+		}
+	}
+}
+/******************************************************************************************************************************************/
+/***********End****************/
