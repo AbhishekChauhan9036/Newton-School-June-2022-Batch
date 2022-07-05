772897c8f2c785ae703cb320c88d4e1043e4913c
Abhi <abhishekchauhan9036@gmail.com>
2022-07-05 06:35:25 +0530

    commit message

---
+++ Control Structures, Loops, Ternary Operator - Post Class/Pattern-Printing.java
@@ -0,0 +1,23 @@
+/* Author = Chauhan Abhishek */
+/* Codechef Id = abhishekchauha */
+/* Codeforces Id = abhishekchauhan903 */
+/* GitHub Id = AbhishekChauhan9036 */
+/* Language = JAVA */
+/* Address = Ballia,UP,INDIA (221716)*/
+
+/******************************************************************************************************************************************/
+/***********Start****************/
+static void pattern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j<=i){
+                System.out.print(j+" ");
+            }else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
+/******************************************************************************************************************************************/
+/***********End****************/
