30f3a94aeff407d7ba9359e12282f829e8de2cf9
Abhi <abhishekchauhan9036@gmail.com>
2022-07-21 09:59:10 +0530

    commit message

---
+++ Double Linked List - In Class/insert-node-at-kth-position-in-doubly-linked-list.java
@@ -0,0 +1,41 @@
+/*
+
+class Node {
+    Node next;
+    Node prev;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+        prev = null;
+    }
+}
+*/
+
+
+
+public static Node insertnew(Node head, int data ,int position){
+//enter your code here
+position=position-1;
+ Node trackedHeadNode = head;
+    
+     Node nodeToInsert = new Node(data); 
+ if(position == 0){nodeToInsert.next = head; 
+ return nodeToInsert;}    
+    
+ int currPosition = 0;
+while (currPosition < position -1 && head.next != null){
+        head = head.next;        
+        currPosition++;       
+    }
+
+  
+    Node nodeAtPosition = head.next;
+    head.next = nodeToInsert;
+    head = head.next;
+    head.next = nodeAtPosition;
+        
+    return trackedHeadNode;
+
+}
